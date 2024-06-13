d2c3c724e10beef6ba8419ee981a316aa851abb3
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-06-13 23:31:38 +0000

    chore: Create migration files

---
+++ src/core/database/migrations/1718321422552-bk_tb_categories.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class BkTbCategories1718321422552 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
