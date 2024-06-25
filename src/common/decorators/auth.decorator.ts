4326a4d66accd2a3200fbaf42424bcacb8740f18
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-06-25 08:25:21 -0300

    chore: Removed websockets lib and remaining code. Closes #96

--- src/common/decorators/auth.decorator.ts
+++ src/common/decorators/auth.decorator.ts
@@ -2,7 +2,6 @@ import { applyDecorators, UseGuards } from '@nestjs/common';
 import { AccessTokenGuard } from '@/common/guards/accessToken.guard';
 import { RefreshTokenGuard } from '@/common/guards/refreshToken.guard';
 import { LocalGuard } from '@/common/guards/Local.guard';
-import { JwtWsGuard } from '@/common/guards/ws.guard';
 
 export function LocalAuth() {
   return applyDecorators(UseGuards(LocalGuard));
@@ -11,9 +10,7 @@ export function LocalAuth() {
 export function Auth() {
   return applyDecorators(UseGuards(AccessTokenGuard));
 }
-export function WsAuth() {
-  return applyDecorators(UseGuards(JwtWsGuard));
-}
+
 
 export function Reauth() {
   return applyDecorators(UseGuards(RefreshTokenGuard));
