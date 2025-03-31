3d3cb76aa14cb48a42d59615c0743bdf6ff5acd8
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-03-30 21:05:42 -0300

    wip: working on parameter features

--- src/modules/shared.module.ts
+++ src/modules/shared.module.ts
@@ -7,12 +7,14 @@ import { PaymentsModule } from './payments/payments.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { CategoriesModule } from './categories/categories.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { ParametersModule } from './parameters/parameters.module';
 @Module({
   imports: [
     HealthModule,
     UsersModule,
     AuthModule,
     PaymentsModule,
+    ParametersModule,
     CategoriesModule,
     AccountsModule,
     TransactionsModule,
