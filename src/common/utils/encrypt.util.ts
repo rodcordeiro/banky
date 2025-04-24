6a09244ebdf64f6a691401de4927bc51a44989bf
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-04-24 10:50:34 -0300

    fix: Criptografia utilizada

--- src/common/utils/encrypt.util.ts
+++ src/common/utils/encrypt.util.ts
@@ -1,10 +1,10 @@
-import { TripleDES, enc, SHA256 } from 'crypto-js';
+import { enc, SHA256, AES } from 'crypto-js';
 import { Authenticate } from '@/common/interfaces/authenticated.interface';
 
 export const EncryptUtils = {
   encrypt: (data: Authenticate.IAuthToken, token: string): string => {
     const key = SHA256(token).toString();
-    const payload = TripleDES.encrypt(JSON.stringify(data), key).toString();
+    const payload = AES.encrypt(JSON.stringify(data), key).toString();
     return payload;
   },
 
@@ -13,7 +13,7 @@ export const EncryptUtils = {
     token: string,
   ): Authenticate.IAuthenticatedUser['user'] => {
     const key = SHA256(token).toString();
-    const payload = TripleDES.decrypt(data, key).toString(enc.Utf8);
+    const payload = AES.decrypt(data, key).toString(enc.Utf8);
     return JSON.parse(payload);
   },
 };
